cdbb2f418874552494e55b75991b2061111babf6
Ryan Thompson <service@aiwebsystems.com>
2014-10-21 09:15:39 -0500

    Namespace

--- src/StreamsDistributionServiceProvider.php
+++ src/StreamsDistributionServiceProvider.php
@@ -10,6 +10,6 @@ class StreamsDistributionServiceProvider extends ServiceProvider
     public function register()
     {
         // Register routes.
-        $this->app->register('Streams\Addon\Distribution\Streams\Provider\RoutesServiceProvider');
+        $this->app->register('Anomaly\Streams\Addon\Distribution\Streams\Provider\RoutesServiceProvider');
     }
 }
