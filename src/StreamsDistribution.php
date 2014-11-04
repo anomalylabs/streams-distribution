ec3368365cffd677c87dc8ca78071f3cf7d837c0
Ryan Thompson <service@aiwebsystems.com>
2014-11-04 01:40:32 -0600

    Renamed

--- src/StreamsDistribution.php
+++ src/StreamsDistribution.php
@@ -1,8 +1,8 @@
 <?php namespace Anomaly\Streams\Addon\Distribution\Streams;
 
-use Anomaly\Streams\Platform\Addon\Distribution\DistributionAddon;
+use Anomaly\Streams\Platform\Addon\Distribution\Distribution;
 
-class StreamsDistribution extends DistributionAddon
+class StreamsDistribution extends Distribution
 {
     public function getAdminTheme()
     {
