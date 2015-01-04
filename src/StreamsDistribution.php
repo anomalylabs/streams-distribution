83858991a30345a3714e2004166b8f653e2b9947
Ryan Thompson <service@aiwebsystems.com>
2015-01-04 12:16:29 -0600

    Doc

--- src/StreamsDistribution.php
+++ src/StreamsDistribution.php
@@ -2,6 +2,14 @@
 
 use Anomaly\Streams\Platform\Addon\Distribution\Distribution;
 
+/**
+ * Class StreamsDistribution
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\StreamsDistribution
+ */
 class StreamsDistribution extends Distribution
 {
 
