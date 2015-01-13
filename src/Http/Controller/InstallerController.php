e5d1e216b027134306ddbcbf280229c27b9a045f
Ryan Thompson <service@aiwebsystems.com>
2015-01-12 18:17:29 -0600

    Load explicitly from streams.

--- src/Http/Controller/InstallerController.php
+++ src/Http/Controller/InstallerController.php
@@ -1,19 +1,38 @@
 <?php namespace Anomaly\StreamsDistribution\Http\Controller;
 
-use Anomaly\StreamsDistribution\Ui\Form\InstallerFormBuilder;
 use Anomaly\Streams\Platform\Http\Controller\PublicController;
+use Anomaly\StreamsDistribution\Ui\Form\InstallerFormBuilder;
 
+/**
+ * Class InstallerController
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\StreamsDistribution\Http\Controller
+ */
 class InstallerController extends PublicController
 {
 
+    /**
+     * Create a new InstallerController instance.
+     *
+     * @param InstallerFormBuilder $form
+     * @return \Illuminate\View\View|\Symfony\Component\HttpFoundation\Response
+     */
     public function index(InstallerFormBuilder $form)
     {
         return $form->render();
     }
 
+    /**
+     * Show the complete page.
+     *
+     * @return \Illuminate\View\View
+     */
     public function complete()
     {
-        return view('distribution::complete');
+        return view('anomaly.distribution.streams::complete');
     }
 }
  
